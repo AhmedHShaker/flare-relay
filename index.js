9eb45a2d42c8aa46f969de9b0de7f66d72287528
Ahmed Shaker <ahmedshaker1996@gmail.com>
2026-02-28 10:45:38 +0300

    Try port 587 STARTTLS

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ app.use(express.json());
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.zoho.com',
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.ZOHO_USER,
     pass: process.env.ZOHO_PASS,
